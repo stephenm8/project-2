51803ab5578d7c42e48bd6d28200ccd1637d959d
Stephen Matthews <stephenm@uoregon.edu>
2020-04-27 16:37:07 -0700

    add revWords1, 2, and 3 to functions.js

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,34 @@
 // jshint esversion: 6
 
+let revWords1 = str => {
+  let result = "";
+  for (var i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+  return result;
+};
+console.log(revWords1("Web app dev"));
+
+let revWords2 = str => {
+  let arr = str.split("");
+  let result = "";
+  arr.forEach(char => {
+    result = char + result;
+  });
+  return result;
+};
+console.log(revWords2("Web app dev"));
+
+let revWords3 = str => {
+  let arr = str.split("");
+  let result = "";
+  for (char of arr) {
+    result = char + result;
+  }
+  return result;
+};
+console.log(revWords3("Web app dev"));
+
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
