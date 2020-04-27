c3c744df0cbf0b73efd3c889f6b2609d45b73d34
Stephen Matthews <stephenm@uoregon.edu>
2020-04-27 16:57:36 -0700

    complete project 2 requirement 1

--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,40 +1,31 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function() {
+  let prefixURL =
+    "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
+  let suffixURL = "&format=json&jsoncallback=?";
+  let flickrTag = $("input").val();
+  let requestURL = prefixURL + flickrTag + suffixURL;
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
-    let suffixURL = "&format=json&jsoncallback=?";
-    //get value entered by user from textbox
-    let flickrTag = $("input").???();
-    let requestURL = prefixURL + flickrTag + suffixURL;
-
-    //clear old photos
-    $(".photos").???("");
+  $(".photos").attr("../images/dog.jpg");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
-
-      //Flickr returns 20 images by default
-      //We need only six images for the Gallery
       if (index < 6) {
-        // create a new JQuery element to hold the image
-        // but hide it so we can fade it in
         let $img = $("<img>").hide();
 
-        // set the attribute to the url
-        // contained in the response
         $img.attr("src", item.media.m);
         $img.attr("width", "100");
 
-        // attach the img tag to the main
-        // photos element and then fade it in
         $(".photos").append($img);
         $img.fadeIn();
       }
     });
-
   });
 };
+//$(document).ready(controller);
 
-
-$(document).ready(main);
+window.addEventListener("load", () => {
+  let button = document.querySelector("button");
+  button.addEventListener("click", controller);
+});
